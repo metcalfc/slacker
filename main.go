1fa2b18f178782d006876d172b38f91ac626c477
Chad Metcalf <metcalfc@gmail.com>
2019-03-21 10:35:31 -0700

    Get go modules working and add slack api.
    Signed-off-by: Chad Metcalf <metcalfc@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/nlopes/slack"
+)
 
 var (
 	version = "dev"
@@ -10,4 +15,17 @@ var (
 
 func main() {
 	fmt.Printf("%v, commit %v, built at %v", version, commit, date)
+
+	SlackToken := os.Getenv("SLACK_TOKEN")
+
+	api := slack.New(SlackToken)
+
+	groups, err := api.GetGroups(false)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	for _, group := range groups {
+		fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
+	}
 }
